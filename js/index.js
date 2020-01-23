259bb39486c4afea319f8d2049733e1b7192e8dd
CristinaHeredia <crisheredian@hotmail.com>
2020-01-23 17:18:48 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,74 @@
+/*
+  Creación de una función personalizada para jQuery que detecta cuando se detiene el scroll en la página
+*/
+$.fn.scrollEnd = function(callback, timeout) {
+  $(this).scroll(function(){
+    var $this = $(this);
+    if ($this.data('scrollTimeout')) {
+      clearTimeout($this.data('scrollTimeout'));
+    }
+    $this.data('scrollTimeout', setTimeout(callback,timeout));
+  });
+};
+/*
+  Función que inicializa el elemento Slider
+*/
+
+function inicializarSlider(){
+  $("#rangoPrecio").ionRangeSlider({
+    type: "double",
+    grid: false,
+    min: 0,
+    max: 100000,
+    from: 200,
+    to: 80000,
+    prefix: "$"
+  });
+}
+/*
+  Función que reproduce el video de fondo al hacer scroll, y deteiene la reproducción al detener el scroll
+*/
+function playVideoOnScroll(){
+  var ultimoScroll = 0,
+      intervalRewind;
+  var video = document.getElementById('vidFondo');
+  $(window)
+    .scroll((event)=>{
+      var scrollActual = $(window).scrollTop();
+      if (scrollActual > ultimoScroll){
+       video.play();
+     } else {
+        //this.rewind(1.0, video, intervalRewind);
+        video.play();
+     }
+     ultimoScroll = scrollActual;
+    })
+    .scrollEnd(()=>{
+      video.pause();
+    }, 10)
+}
+
+inicializarSlider();
+playVideoOnScroll();
+
+$('#mostrarTodos').click(function(event){
+  alert ("funciona el botón");
+  //document.getElementByClassName("colContenido").innerHTML='<img src="../img/home.jpg" />';
+  //$(document).ready(function(){
+
+            event.preventDefault();
+            $.ajax({
+                url: "./data-1.json",
+                type: "POST",
+                cache: false,
+                success: function(data){
+                    console.log(data);
+                },
+                error: function(data){
+                    console.log("error");
+                }
+            });
+
+    //});
+})
+//<img src="img/home.jpg" width="200" height="150">
